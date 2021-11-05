48edef5acb6d7ad47591358e8bf33dcb6160f7c6
deepratnaawale <awale.deep@gmail.com>
2021-11-05 23:34:56 +0530

    Basic working version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# You-Are-Flat-Now
- Video to Panaroma Creator
+# Image and Video Stitching
+This algorithm runs through a video file, or a set of images, and stitches them together to form a single image. It can be
+used for scanning in large documents where the resolution from a single photo may not be sufficient. 
+
+## In Progress
+Fix Image Blurring
+Evaluating frame Quality
+Lens Distortion
+
+## Quick Start
+Clone > Get Requirements > Run!
+
+## Requirements
+numpy
+opencv-python>=4.5
+opencv-contrib-python>=4.5
+yapf
+pylint
+
+```bash
+# Clone the repo
+git clone https://github.com/deepratnaawale/You-Are-Flat-Now && cd ImageStitching
+
+# install deps
+pip install -r requirements.txt
+
+# Run the stitching!
+python stitching.py <path to image directory or video files> --display --save
+```
+
+## Demonstration
+Coming soon!
+
+## References
+[Automatic Panoramic Image Stitching using Invariant Features](https://www.cs.bath.ac.uk/brown/papers/ijcv2007.pdf)
---
+++ image_stitching/.ipynb_checkpoints/helpers-checkpoint.py
@@ -0,0 +1,58 @@
+import logging
+import pathlib
+
+from typing import List
+from typing import Generator
+
+import cv2
+import numpy
+
+__doc__ = '''helper functions for loading frames and displaying them'''
+
+
+def display(title, img, max_size=500000):
+    '''
+    resizes the image before it displays it,
+    this stops large stitches from going over the screen!
+    '''
+    assert isinstance(img, numpy.ndarray), 'img must be a numpy array'
+    assert isinstance(title, str), 'title must be a string'
+    scale = numpy.sqrt(min(1.0, float(max_size) / (img.shape[0] * img.shape[1])))
+    shape = (int(scale * img.shape[1]), int(scale * img.shape[0]))
+    img = cv2.resize(img, shape)
+    cv2.imshow(title, img)
+
+
+
+    
+def read_video(video_path: pathlib.Path):
+
+    '''read video is a generator class yielding frames'''
+    cap = cv2.VideoCapture(str(video_path))
+
+    while True:
+        ret, frame = cap.read()
+        if not ret or frame is None:
+            break
+
+        yield frame
+    
+
+
+
+def load_frames(paths: List[str]) -> Generator[numpy.ndarray, None, None]:
+    '''
+    load_frames takes in a list of paths to image,
+    video files, or directories and yields them
+    '''
+    for path in paths:
+        path = pathlib.Path(path)
+
+        if path.is_dir():
+            yield from load_frames(path.rglob('*'))
+        elif path.suffix.lower() in ['.jpg', '.jpeg', '.png']:
+            yield cv2.imread(str(path))
+        elif path.suffix.lower() in ['.avi', '.mp4', '.mov']:
+            yield from read_video(path)
+        else:
+            logging.warning(f'skipping {path.name}...')
